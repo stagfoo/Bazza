a1d7c0ff51263f45c5c7471945b68a5755672d62
King, Alexander <alexander.king@rakuten.com>
2017-03-07 16:33:47 +0900

    Added info scrapper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import shapeshift from './grid';
 import './jquery-ui'
+$.ajax({
+  url: 'https://www.google.co.jp/?gfe_rd=cr&ei=IWK-WJuaCoX-8wfXn7DACA&gws_rd=ssl',
+  async: true,
+  success: function(data) {
+    var matches = data.match(/<title>(.*?)<\/title>/);
+    alert(matches[0]);
+  }
+});
 
 $.getJSON("index.json", function (json) {
     console.log(json); // this will show the info it in firebug console
