4c568cec2d85d3c0be138d833deaf06e3d71bc5f
King, Alexander <alexander.king@rakuten.com>
2017-03-07 16:23:44 +0900

    example working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import shapeshift from './grid';
+import './jquery-ui'
+
 $.getJSON("index.json", function (json) {
     console.log(json); // this will show the info it in firebug console
     Object.keys(json).map(function(key, index) {
         // json[key]
-        $("main").append('<div class="container"></div>');
+        var container = $("<div>", { "class": "container" });
+        if (json[key].length > 0) {
+            json[key].map(function (key, index) {
+                container.append($("<div>", { "class": "child" }))
+            });
+        }
+        $("main").append(container);
+
     });
      $(".container").shapeshift({
          minColumns: 3,
