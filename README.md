7c5f950cde4b2ed29a89ea98388d457ef4f66ab1
King, Alexander <alexander.king@rakuten.com>
2017-03-08 09:26:56 +0900

    added local storage

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bazza
-A Locally open tab manager for 
+A Local Bookmark/Tab manager for sensitive data.
+
+## Todo
+- local storage saving
+- move to vue for sane dom manage
+- export function for all tabs
+- export function for one group
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './jquery-ui'
 function createChild(css, obj) {
     const tabTag = $("<div>", { "class": css });
     const wrapper = $("<a>", { "class": "wrapper","href": obj.url  });
-    wrapper.append($("<img>", 
-        { 
+    wrapper.append($("<img>",
+        {
             "src": obj.favIconUrl,
              "height":"30px",
              "width":"30px"
-             
+
         }));
     wrapper.append($("<span contenteditable='true'>"+obj.title+"</span>"));
     tabTag.append(wrapper);
@@ -24,12 +24,12 @@ function createRow(extraClass){
 
 
 function createTabs(){
-// Do NOT forget that the method is ASYNCHRONOUS    
+// Do NOT forget that the method is ASYNCHRONOUS
 chrome.tabs.query({
     lastFocusedWindow: true     // In the current window
     }, function(tabs) {
         const container = createRow('tabs');
-        
+
         tabs.map(function(tab, index) {
             const tabTag = createChild('child tab', tab);
             container.append(tabTag)
@@ -45,7 +45,7 @@ $.getJSON("index.json", function (json) {
         var container = createRow('group');
         if (json[key].length > 0) {
             container.append($("<h1>"+key+"</h1>"))
-            
+
             json[key].map(function (data, index) {
                 const child = createChild('child',data);
                 container.append(child);
@@ -60,12 +60,25 @@ $.getJSON("index.json", function (json) {
         colWidth: 200
     });
 });
+
 function createJson(){
     const groups = document.getElementsByClassName('group');
     for (var i = 0; i < groups.length; ++i) {
-    const group = groups[i];   
+    const group = groups[i];
     const groupObj = {};
     groupObj.name = group.children[0];
     }
 }
-createJson();
+function save(object) {
+    chrome.storage.local.set(object);
+}
+function load(){
+    chrome.storage.local.get(null, function (result) {
+        return result;
+  });
+}
+function loadByKey(key){
+    chrome.storage.local.get(key, function (result) {
+        return result;
+  });
+}
