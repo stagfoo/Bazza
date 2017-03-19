a232ff1655e9247e8450d84796a1abeb37f84e81
Helg, Luke <luke.helg@rakuten.com>
2017-03-19 20:04:56 +0900

    lint app

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const router = require('./store/router')
 const states = require('./store/states')
 const reducers = require('./store/reducers')
 const effects = require('./store/effects')
-const styles = require('./styles/global')
+
 // App Attributes
 app.model({ state: states, reducers: reducers, effects: effects })
 app.router(router)
 // Logger
 
-//Render App
+// Render App
 const tree = app.start()
 document.body.appendChild(tree)
