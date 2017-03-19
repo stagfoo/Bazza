8bb5fdc2b80cb1ccb07b4ceee353cf52aff71206
Helg, Luke <luke.helg@rakuten.com>
2017-03-19 20:03:11 +0900

    lint styles

--- styles/global.js
+++ styles/global.js
@@ -1,30 +1,34 @@
-const cssConst = {
-    'black': '#282828',
-    'white': '#fff',
-    'grey': '#ebebeb'
-}
 const fairybread = require('fairybread')
-const globalCss = new fairybread('global')
-globalCss.add('body',`
+const globalCss = new fairybread('global') // eslint-disable-line new-cap
+
+const cssConst = {
+  black: '#282828',
+  white: '#fff',
+  grey: '#ebebeb'
+}
+
+globalCss.add('body', `
    margin:0px;
    padding:0px;
    font-size:10px;
 `)
-globalCss.add('input',`
+
+globalCss.add('input', `
    margin:0px;
    padding:0px;
    border:0px;
    font-size:1.7em;
 `)
 // Global Styles
-globalCss.add('.group',`
+globalCss.add('.group', `
     background:${cssConst.grey};
     width:100%;
     display:block;
     float:left;
     height:200px;
 `)
-globalCss.add('.card',`
+
+globalCss.add('.card', `
     border-radius: 4px;
     background-color: ${cssConst.white};
     border-bottom: 2px solid rgba(0, 0, 0, 0.08);
@@ -32,5 +36,3 @@ globalCss.add('.card',`
     margin:10px;
     padding:10px;
 `)
-
-
