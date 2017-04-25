e10a16b083dfd5897797e69e260099d43aec6197
Alex King <alexanderddking@gmail.com>
2017-04-26 06:28:54 +0900

    Sutble Styling

--- src/styles/controls.js
+++ src/styles/controls.js
@@ -1,4 +1,9 @@
 function styles(sheet, sv) {
-
+  sheet.add('main > nav', `
+    border-bottom:1px solid rgba(0,0,0,0.14);
+    width:80.4vw;
+    padding:0 0.3vw;
+    margin-bottom:1em;
+  `)
 }
 module.exports = styles
